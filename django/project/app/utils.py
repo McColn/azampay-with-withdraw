822b69401cb4ff41fca4047f38b937b714229835
McColn <102020324+McColn@users.noreply.github.com>
2025-03-12 15:10:57 +0300

    Update utils.py

--- django/project/app/utils.py
+++ django/project/app/utils.py
@@ -11,9 +11,9 @@ def get_azampay_token():
     url = "https://authenticator-sandbox.azampay.co.tz/AppRegistration/GenerateToken"
     headers = {"Content-Type": "application/json"}
     payload = {
-        "appName": "McColn",
-        "clientId": "f5f66619-bfd8-4375-8b79-0b7caa642692",
-        "clientSecret": "UolIKZlt6xsQN+sChnuHXCbcq8eosrhXqVrHQPHZN8wf0fqw38I0utYzTboLiGKvUfOJefs9P5CCzxk5dAD9y9N4+i+AQUwiV5H/Bh+7Qf8bnC0Rlab4JKa+NVhajWbMhItY8/+KDhAK2rRMx2Matt1DhCZOJhksKcnn1FYTlJTuxdUUrthihjLYeNl0vgltbJrJ3tXKiM/0PNZo2IsvqxH08MQAE8GOP2LOYeoCzSh1V+U1GPva00nwnDJ4R1kmwRweAOj7YE+MdpV36SK92yjJx1hm8k0qnml72S3Wbr+oKZRnOKG7FxBcv3J4evOpK4RijaI2hCoBxHtSaWPFghg+bVtfpZJExbrYXGhQ0TjmjSj5AmSatA7ohoUvM468mW0GfDMnPJg/ycNsC/j53MbK5RGK/0XVf4hYhen3A+udqDpSNz7ptByim21RAc+AuC7u5s7SJKZT6Ten/lbFMDgu8BUcK9WxP/DR5cPRiEwvGA28pvKrKnwM1NcEiDK8s2q/1ngYXZTNo6FkmraShohG3dui+D0RnU8sTWMjz2x2QkghPn/40plkIv6nMbtjtnHGZTns3Wi7YBsoGHrrn7uprQZGq4y/yS7WmqNrTBJ88s5QL4OtK/+IUZeySqkh2PR6B7yIp9hTFkAnisg5SPl8/DCxTN5Kkxsg3INE53g="
+        "appName": "",
+        "clientId": "",
+        "clientSecret": ""
     }
 
     response = requests.post(url, json=payload, headers=headers)
